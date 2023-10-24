0d506c8ccff5990bc3fdab460c5d6dd80b860dc8
bbgard <bgardiner@students.federation.edu.au>
2023-10-25 10:26:07 +1100

    Added all thumbnails, watchCounts, localStorage

--- src/App.js
+++ src/App.js
@@ -1,98 +1,115 @@
-// import logo from './logo.svg';
-import "./App.css";
-import * as React from "react";
+import React, { useEffect, useState } from "react";
 import {
   Container,
   Typography,
   Box,
-  Link,
   Button,
   Card,
   CardActions,
   CardContent,
   CardMedia,
 } from "@mui/material";
-import ResponsiveAppBar from "./Components/ResponsiveAppBar";
-import workouts from "./Data/links";
+import {workouts, workoutSchedule} from "./Data/workouts";
+import { Copyright } from "./Components/Copyright";
+import { ResponsiveAppBar } from "./Components/ResponsiveAppBar";
 
-function Copyright() {
-  return (
-    <Typography variant="body2" color="white" align="center">
-      {"Copyright © "}
-      <Link color="inherit" href="https://mui.com/">
-        Benjamin Gardiner
-      </Link>{" "}
-      {new Date().getFullYear()}
-      {"."}
-    </Typography>
-  );
-}
 
 function App() {
-  // Initial workout schedule
-  const workoutSchedule = [
-    {
-      day: "Monday",
-      workout: "Legs",
-    },
-    {
-      day: "Tuesday",
-      workout: "Chest",
-    },
-    {
-      day: "Wednesday",
-      workout: "Back",
-    },
-    {
-      day: "Thursday",
-      workout: "Shoulders",
-    },
-    {
-      day: "Friday",
-      workout: "Arms",
-    },
-    {
-      day: "Saturday",
-      workout: "Abs",
-    },
-    {
-      day: "Sunday",
-      workout: "Rest",
-      duration: "Rest",
-    },
-  ];
-
   // Pick a random workout from the workout schedule
-  const [recommendedWorkout, setRecommendedWorkout] = React.useState(
-    workouts.filter(
-      (workout) => workout.category === workoutSchedule[new Date().getDay()].workout
-    )[Math.floor(Math.random() * workouts.filter((workout) => workout.category === workoutSchedule[new Date().getDay()].workout).length)]
-  );
+  const [recommendedWorkout, setRecommendedWorkout] = useState([]);
+  const [todaysWorkouts, setTodaysWorkouts] = useState([]);
+  const [allWorkouts, setAllWorkouts] = useState([]);
 
-  // Regenerate a random workout from the workout schedule
-  function regenerateRecommendedWorkout() {
-        setRecommendedWorkout(
-          workouts.filter(
-            (workout) =>
-              workout.category === workoutSchedule[new Date().getDay()].workout
-          )[
-            Math.floor(
-              Math.random() *
-                workouts.filter(
-                  (workout) =>
-                    workout.category ===
-                    workoutSchedule[new Date().getDay()].workout
-                ).length
-            )
-          ]
-        );
+  // On mount, load the workouts from localStorage or use the default workouts
+  useEffect(() => {
+    // Check if there is a local copy of workouts
+    const savedAllWorkouts = JSON.parse(localStorage.getItem("allWorkouts"));
+
+    if (savedAllWorkouts) {
+      console.log("Loading saved workouts");
+      setAllWorkouts(savedAllWorkouts);
+
+      // Generate today's workouts and recommended workout
+      generateTodaysWorkouts(savedAllWorkouts);
+    } else {
+      console.log("Loading default workouts");
+      setAllWorkouts(workouts);
+
+      // Generate today's workouts and recommended workout
+      generateTodaysWorkouts(workouts);
+
+      // Save the default workouts to local storage
+      localStorage.setItem("allWorkouts", JSON.stringify(workouts));
+    }
+  }, []);
+
+  // Generate today's workouts and recommended workout
+  const generateTodaysWorkouts = (workoutData) => {
+    const lastGeneratedTimestamp = localStorage.getItem("lastGeneratedTimestamp");
+    const currentDate = new Date();
+
+    // Check if the workouts need to be regenerated
+    if (
+      todaysWorkouts.length === 0 ||
+      !lastGeneratedTimestamp ||
+      currentDate.getDate() !== new Date(lastGeneratedTimestamp).getDate()
+    ) {
+      console.log("Regenerating workouts");
+
+      const workoutList = workoutData.filter(
+        (workout) =>
+          workout.category.includes(workoutSchedule[currentDate.getDay()].workout)
+      );
+
+      workoutList.sort((a, b) => (a.watchCount > b.watchCount ? 1 : -1));
+
+      setTodaysWorkouts(workoutList);
+
+      // Store the current date as the last generated timestamp
+      localStorage.setItem("lastGeneratedTimestamp", currentDate);
+
+      // Set the recommended workout
+      setRecommendedWorkout(workoutList[0]);
+    }
+  };
+
+  // Save all workouts to localStorage whenever they change
+  useEffect(() => {
+    console.log("Saving workouts");
+    localStorage.setItem("allWorkouts", JSON.stringify(allWorkouts));
+    console.log("Workouts saved", allWorkouts);
+  }, [allWorkouts]);
+
+  // Increment the recommended workout
+  const incrementRecommendedWorkout = () => {
+    const currentWorkoutIndex = todaysWorkouts.indexOf(recommendedWorkout);
+
+    if (currentWorkoutIndex < todaysWorkouts.length - 1) {
+      setRecommendedWorkout(todaysWorkouts[currentWorkoutIndex + 1]);
+    }
+    else {
+      setRecommendedWorkout(todaysWorkouts[0]);
+    }
+
+    // Print the watch count to the console
+    console.log(recommendedWorkout.watchCount);
   }
 
   // Watch the selected workout
   function watchSelectedWorkout() {
+    // Increment the watch count
+    recommendedWorkout.watchCount++;
+
+    // Update the watch count in the allWorkouts array
+    const workoutIndex = allWorkouts.indexOf(recommendedWorkout);
+    allWorkouts[workoutIndex].watchCount = recommendedWorkout.watchCount;
+
+     // Save the updated allWorkouts to local storage
+     localStorage.setItem("allWorkouts", JSON.stringify(allWorkouts));
+
+
     window.open(recommendedWorkout.link, "_blank");
   }
-
 
   return (
     <Container
@@ -101,7 +118,7 @@ function App() {
     >
       <ResponsiveAppBar />
 
-      <Box sx={{ my: 4, height: "100%", maxHeight: "70vh"}}>
+      <Box sx={{ my: 4, height: "100%", maxHeight: "70vh" }}>
         <Typography
           variant="h4"
           component="h1"
@@ -114,16 +131,14 @@ function App() {
           {workoutSchedule[new Date().getDay()].workout}
         </Typography>
 
-        <Card sx={{ maxWidth: 345, margin: "1rem auto"}}>
+        <Card sx={{ maxWidth: 345, margin: "1rem auto" }}>
           <CardMedia
             component="img"
             alt="workout video screenshot"
             height="190"
-            //grab image from recommendedWorkout.link
-            // image="https://i.ytimg.com/vi_webp/b0Z81IGUnGI/sddefault.webp"
             image={recommendedWorkout.thumbnail}
           />
-          <CardContent sx={{textAlign: "center", margin: "0 auto", height: "15vh"}}>
+          <CardContent sx={{ textAlign: "center", margin: "0 auto", height: "15vh" }}>
             <Typography gutterBottom variant="h5" component="div">
               Recommended Workout
             </Typography>
@@ -132,13 +147,13 @@ function App() {
               Duration: {recommendedWorkout.duration}
             </Typography>
           </CardContent>
-          <CardActions sx={{justifyContent: "center"}}>
-            <Button size="small" onClick={regenerateRecommendedWorkout}>Next</Button>
+          <CardActions sx={{ justifyContent: "center" }}>
+            <Button size="small" onClick={incrementRecommendedWorkout}>Next</Button>
             <Button size="small" onClick={watchSelectedWorkout}>Watch</Button>
           </CardActions>
         </Card>
 
-        <Copyright/>
+        <Copyright />
       </Box>
     </Container>
   );
