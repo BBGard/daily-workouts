a5813f8baddbc7f3fe05e1b7190ff416c7df8e88
bbgard <bgardiner@students.federation.edu.au>
2023-11-19 17:34:19 +1100

    Added placeholder image for stretches page

--- src/Pages/Stretches.jsx
+++ src/Pages/Stretches.jsx
@@ -1,14 +1,23 @@
 
 import React from 'react';
+import { Box } from '@mui/material';
 import yogaImage from '../images/undraw_yoga_re_i5ld.svg'
 
 const Stretches = () => {
   return (
     // add yoga image
-    <div>
-      <h1>Stretches</h1>
+    <Box
+      sx={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        flexDirection: "column",
+        height: "100vh",
+        width: "100vw",
+      }}
+      >
       <img src={yogaImage} alt="yoga" />
-    </div>
+    </Box>
   );
 };
 
