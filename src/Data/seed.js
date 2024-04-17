948480e871a0c7324bad2e970e45245510d3bc34
bbgard <gonzogardiner@gmail.com>
2024-04-17 15:21:30 +1000

    add supabase public key

--- src/Data/seed.js
+++ src/Data/seed.js
@@ -10,9 +10,8 @@ const { env } = require('process');
 const supabaseUrl = 'https://bvttwoglfprfoynqkqqb.supabase.co'; // Supabase URL
 
 // Supabase Key - anon public key (safe in browsers as long as you have RLS enabled)
-// const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJ2dHR3b2dsZnByZm95bnFrcXFiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTMwNzYwNDEsImV4cCI6MjAyODY1MjA0MX0.dT1L-B_hejEIfDWbk9pE1uiFcXMk-hub3A3NBHFRdSE'
-const supabaseKey = `${process.env.temp.SUPA_KEY}`;
-console.log("key : ", supabaseKey);
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJ2dHR3b2dsZnByZm95bnFrcXFiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTMwNzYwNDEsImV4cCI6MjAyODY1MjA0MX0.dT1L-B_hejEIfDWbk9pE1uiFcXMk-hub3A3NBHFRdSE'
+
 const supabase = createClient(supabaseUrl, supabaseKey); // Create a Supabase client
 
 
