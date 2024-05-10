e1e2bf4670169aa5a5442c3bdadaf4d28c0f10f3
bbgard <bgardiner@students.federation.edu.au>
2024-05-10 14:33:38 +1000

    Refactor useGetWorkoutData hook to retutn object containing workout data and functions

--- src/hooks/useGetWorkoutData.js
+++ src/hooks/useGetWorkoutData.js
@@ -17,7 +17,6 @@ async function getAllWorkoutData() {
 }
 
 export const useGetWorkoutData = () => {
-  const [allWorkoutData, setAllWorkoutData] = useState();
   const [cachedWorkoutData, setCachedWorkoutData] = useState(null);
   const [recommendedWorkout, setRecommendedWorkout] = useState([]);
   const [todaysWorkouts, setTodaysWorkouts] = useState([]);
@@ -189,8 +188,8 @@ export const useGetWorkoutData = () => {
 
   }, [currentWorkoutSchedule, cachedWorkoutData]);
 
-  return {
-    allWorkoutData,
+
+  const workoutData = {
     recommendedWorkout,
     todaysWorkouts,
     warmups,
@@ -200,88 +199,13 @@ export const useGetWorkoutData = () => {
     recommendedRecovery,
     recommendedStretch,
     currentWorkoutSchedule,
+    workoutScheduleAlt,
     incrementRecommendedRecovery,
     incrementRecommendedStretch,
     incrementRecommendedWarmup,
     switchCurrentWorkoutSchedule,
     incrementRecommendedWorkout,
-    workoutScheduleAlt,
-  };
+  }
+
+  return workoutData;
 };
-
-// import { useEffect, useState } from 'react';
-// import { db } from '../Config/firebase';
-// import { collection, onSnapshot, query } from 'firebase/firestore';
-
-// export const useGetWorkoutData = () => {
-//   const [allWorkoutData, setAllWorkoutData] = useState();
-
-//   useEffect(() => {
-//     const getWorkoutData = async () => {
-//       const workoutCollectionRef = collection(db, 'workouts'); // Get all workouts
-
-//       let data = {};
-//       let unsubscribe; // Unsubscribe from db when component unmounts
-
-//       try {
-//         console.log("pulling data");
-//         const queryWorkouts = query(workoutCollectionRef);
-
-//         // Subscribe to db
-//         unsubscribe = onSnapshot(queryWorkouts, (snapshot) => {
-//           const workouts = [];
-//           snapshot.forEach((doc) => {
-//             workouts.push(doc.data());
-//           });
-
-//           // Add workouts array to data object
-//           data = { workouts };
-//           setAllWorkoutData(data);
-
-//           // Cache data in local storage with a 1 hour expiration
-//           const now = new Date();
-//           const item = {
-//             data: data,
-//             timestamp: now.getTime(),
-//             expiration: now.getTime() + 3600000,
-//           };
-//           localStorage.setItem('workouts', JSON.stringify(item));
-//         });
-
-//         // console.log('Workout data retrieved from db');
-//         // console.log(allWorkoutData);
-
-//       } catch (error) {
-//         console.error(error);
-//       }
-
-//       // Return unsubscribe function to prevent memory leaks
-//       return () => unsubscribe();
-//     };
-
-//     // Check if data is cached in local storage, if not pull from db
-//     const cachedWorkoutData = localStorage.getItem('workouts');
-//     if (cachedWorkoutData) {
-//       console.log("cached data found!");
-
-//       const now = new Date();
-//       const cachedData = JSON.parse(cachedWorkoutData);
-
-//       // Check if cached data is expired
-//       if (now.getTime() > cachedData.expiration) {
-//         console.log("cached data expired");
-//         localStorage.removeItem('workouts');
-//         getWorkoutData();
-//         return;
-//       }
-
-//       // Set data from cache
-//       setAllWorkoutData(JSON.parse(cachedWorkoutData));
-//       return;
-//     }
-
-//     getWorkoutData();
-//   }, []);
-
-//   return {allWorkoutData}
-// };
