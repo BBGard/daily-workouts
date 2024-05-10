c32b26fd6b338f5c5c32e1d78072c5021de18cdc
bbgard <bgardiner@students.federation.edu.au>
2024-05-10 14:27:50 +1000

    Feat: added caching to useGetWorkoutData to prevent too many db queries

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -8,172 +8,52 @@ import {
   Tab,
   Divider,
 } from "@mui/material";
-import { workouts, workoutSchedule, workoutScheduleAlt } from "../Data/workoutData";
+// import { workouts, workoutSchedule, workoutScheduleAlt } from "../Data/workoutData";
 // import { workoutSchedule, workoutScheduleAlt } from "../Data/workoutData";
 import WorkoutCard from "../Components/WorkoutCard";
-// import { useGetWorkoutData } from "../hooks/useGetWorkoutData";
+import { useGetWorkoutData } from "../hooks/useGetWorkoutData";
 
 
 const Home = () => {
   // Pick a random workout from the workout schedule
-  const [recommendedWorkout, setRecommendedWorkout] = useState([]);
-  const [todaysWorkouts, setTodaysWorkouts] = useState([]);
-  const [warmups, setWarmups] = useState();
-  const [recovery, setRecovery] = useState();
-  const [stretches, setStretches] = useState();
-  const [recommendedWarmup, setRecommendedWarmup] = useState([]);
-  const [recommendedRecovery, setRecommendedRecovery] = useState([]);
-  const [recommendedStretch, setRecommendedStretch] = useState([]);
-  const [currentWorkoutSchedule, setCurrentWorkoutSchedule] = useState(workoutSchedule);
+  // const [recommendedWorkout, setRecommendedWorkout] = useState([]);
+  // const [todaysWorkouts, setTodaysWorkouts] = useState([]);
+  // const [warmups, setWarmups] = useState();
+  // const [recovery, setRecovery] = useState();
+  // const [stretches, setStretches] = useState();
+  // const [recommendedWarmup, setRecommendedWarmup] = useState([]);
+  // const [recommendedRecovery, setRecommendedRecovery] = useState([]);
+  // const [recommendedStretch, setRecommendedStretch] = useState([]);
+  // const [currentWorkoutSchedule, setCurrentWorkoutSchedule] = useState(workoutSchedule);
   const [selectedTab, setSelectedTab] = useState(0);
-  // const { workouts } =  useGetWorkoutData();
 
 
   // Console log user details
+  const {
+    allWorkoutData,
+    recommendedWorkout,
+    recommendedWarmup,
+    recommendedRecovery,
+    recommendedStretch,
+    currentWorkoutSchedule,
+    switchCurrentWorkoutSchedule,
+    incrementRecommendedWorkout,
+    incrementRecommendedWarmup,
+    incrementRecommendedRecovery,
+    incrementRecommendedStretch,
+    workoutScheduleAlt,
+  } = useGetWorkoutData();
 
 
-  // On mount, load all workouts, generate today's workouts, and set the recommended workout
-  useEffect(() => {
-
-    if (!workouts || workouts.length === 0) return; // Check if workout data is available
-
-    console.log("here is the workout data: ", workouts);
-
-    // Setup Data
-    setWarmups(workouts?.filter(workout => workout.category.includes("Warm Up")));
-    setRecommendedWarmup(workouts?.filter(workout => workout.category.includes("Warm Up"))[0]);
-    setRecovery(workouts?.filter(workout => workout.category.includes("Recovery")));
-    setRecommendedRecovery(workouts?.filter(workout => workout.category.includes("Recovery"))[0]);
-    setStretches(workouts?.filter(workout => workout.category.includes("Stretch")));
-    setRecommendedStretch(workouts?.filter(workout => workout.category.includes("Stretch"))[0]);
 
 
 
-    // Generate today's workouts and recommended workout based on schedule
-    const generateTodaysWorkouts = (workoutData) => {
-      const currentDate = new Date();
-
-        // Filter the workouts based on the current day and schedule
-        const workoutList = workoutData.filter(
-          (workout) =>
-            workout.group.includes(
-              // Javascript uses 0-6 for Sunday-Saturday
-              currentWorkoutSchedule[currentDate.getDay()].group
-            ) &&
-            workout.category.includes(
-              // Javascript uses 0-6 for Sunday-Saturday
-              currentWorkoutSchedule[currentDate.getDay()].category
-            )
-        );
-
-
-        // Create a "score" for each workout based on watch count and rating
-        workoutList.forEach((workout) => {
-          workout.score = workout.rating - workout.watchCount;
-        });
-
-        // Sort the workouts by score
-        workoutList.sort((a, b) => (a.score > b.score ? -1 : 1));
-
-        // Do the same for the warmups, recovery, and stretches
-        const warmupList = workouts.filter(workout => workout.category.includes("Warm Up"));
-        const recoveryList = workouts.filter(workout => workout.category.includes("Recovery"));
-        const stretchList = workouts.filter(workout => workout.category.includes("Stretch"));
-
-        warmupList.forEach((workout) => {
-          workout.score = workout.rating - workout.watchCount;
-        }
-        );
-        warmupList.sort((a, b) => (a.score > b.score ? -1 : 1));
-
-        recoveryList.forEach((workout) => {
-          workout.score = workout.rating - workout.watchCount;
-        }
-        );
-        recoveryList.sort((a, b) => (a.score > b.score ? -1 : 1));
-
-        stretchList.forEach((workout) => {
-          workout.score = workout.rating - workout.watchCount;
-        }
-        );
-        stretchList.sort((a, b) => (a.score > b.score ? -1 : 1));
-
-        // Set the recommended warmup, recovery, and stretch to the first workout in the list
-        setRecommendedWarmup(warmupList[0]);
-        setRecommendedRecovery(recoveryList[0]);
-        setRecommendedStretch(stretchList[0]);
-
-        setTodaysWorkouts(workoutList);
-
-        // Set the recommended workout to the first workout in the list
-        setRecommendedWorkout(workoutList[0]);
-      }
-
-
-     // Generate today's workouts and recommended workout
-     generateTodaysWorkouts(workouts);
-  }, [workouts, todaysWorkouts.length, currentWorkoutSchedule]);
-
-
-  // Increment the recommended workout
-  const incrementRecommendedWorkout = () => {
-    const currentWorkoutIndex = todaysWorkouts.indexOf(recommendedWorkout);
-
-    if (currentWorkoutIndex < todaysWorkouts.length - 1) {
-      setRecommendedWorkout(todaysWorkouts[currentWorkoutIndex + 1]);
-    } else {
-      setRecommendedWorkout(todaysWorkouts[0]);
-    }
-  };
-
-  // Increment the recommended warmup
-  const incrementRecommendedWarmup = () => {
-    const currentWarmupIndex = warmups.indexOf(recommendedWarmup);
-
-    if (currentWarmupIndex < warmups.length - 1) {
-      setRecommendedWarmup(warmups[currentWarmupIndex + 1]);
-    } else {
-      setRecommendedWarmup(warmups[0]);
-    }
-  }
-
-  // Increment the recommended recovery
-  const incrementRecommendedRecovery = () => {
-    const currentRecoveryIndex = recovery.indexOf(recommendedRecovery);
-
-    if (currentRecoveryIndex < recovery.length - 1) {
-      setRecommendedRecovery(recovery[currentRecoveryIndex + 1]);
-    } else {
-      setRecommendedRecovery(recovery[0]);
-    }
-  }
-
-  // Increment the recommended stretch
-  const incrementRecommendedStretch = () => {
-    const currentStretchIndex = stretches.indexOf(recommendedStretch);
-
-    if (currentStretchIndex < stretches.length - 1) {
-      setRecommendedStretch(stretches[currentStretchIndex + 1]);
-    } else {
-      setRecommendedStretch(stretches[0]);
-    }
-  }
 
 
 
   // Toggle between the two workout schedules
   function toggleWorkoutSchedule() {
-
-    // If the current workout schedule is the default schedule, use the alternative schedule
-    if (currentWorkoutSchedule === workoutSchedule) {
-      setCurrentWorkoutSchedule(workoutScheduleAlt);
-    } else {
-      // Otherwise, use the default schedule
-      setCurrentWorkoutSchedule(workoutSchedule);
-    }
-
-    // Store the current workout schedule in local storage
-    localStorage.setItem("workoutSchedule", JSON.stringify(currentWorkoutSchedule));
+    switchCurrentWorkoutSchedule();
   }
 
   // Handle tab selection
@@ -216,6 +96,7 @@ const Home = () => {
             incrementFunction={incrementRecommendedWorkout}
             hasToggle={true}
             toggleFunction={toggleWorkoutSchedule}
+            // isChecked={currentWorkoutSchedule === workoutScheduleAlt}
             isChecked={currentWorkoutSchedule === workoutScheduleAlt}
           />
         )}
--- src/hooks/useGetWorkoutData.js
+++ src/hooks/useGetWorkoutData.js
@@ -1,14 +1,13 @@
-import { useEffect, useState } from 'react';
-import { supabase } from '../Config/supabase.config';
+import { useEffect, useState } from "react";
+import { supabase } from "../Config/supabase.config";
 import { workoutSchedule, workoutScheduleAlt } from "../Data/workoutData";
 
-
- async function getAllWorkoutData() {
+async function getAllWorkoutData() {
   console.log("grabbing data");
 
   const { data, error } = await supabase.from("workouts").select();
 
-  if(error) {
+  if (error) {
     console.error(error);
     return;
   }
@@ -17,9 +16,9 @@ import { workoutSchedule, workoutScheduleAlt } from "../Data/workoutData";
   return data;
 }
 
-
 export const useGetWorkoutData = () => {
   const [allWorkoutData, setAllWorkoutData] = useState();
+  const [cachedWorkoutData, setCachedWorkoutData] = useState(null);
   const [recommendedWorkout, setRecommendedWorkout] = useState([]);
   const [todaysWorkouts, setTodaysWorkouts] = useState([]);
   const [warmups, setWarmups] = useState();
@@ -28,24 +27,187 @@ export const useGetWorkoutData = () => {
   const [recommendedWarmup, setRecommendedWarmup] = useState([]);
   const [recommendedRecovery, setRecommendedRecovery] = useState([]);
   const [recommendedStretch, setRecommendedStretch] = useState([]);
-  const [currentWorkoutSchedule, setCurrentWorkoutSchedule] = useState(workoutSchedule);
-  const [selectedTab, setSelectedTab] = useState(0);
-  const { workouts } =  useGetWorkoutData();
+  const [currentWorkoutSchedule, setCurrentWorkoutSchedule] =
+    useState(workoutSchedule);
 
+  // If the current workout schedule is the default schedule, use the alternative schedule
+  const switchCurrentWorkoutSchedule = () => {
+    if (currentWorkoutSchedule === workoutSchedule) {
+      setCurrentWorkoutSchedule(workoutScheduleAlt);
+    } else {
+      // Otherwise, use the default schedule
+
+      setCurrentWorkoutSchedule(workoutSchedule);
+    }
+  };
+
+  // Increment the recommended workout
+  const incrementRecommendedWorkout = () => {
+    const currentWorkoutIndex = todaysWorkouts.indexOf(recommendedWorkout);
+
+    if (currentWorkoutIndex < todaysWorkouts.length - 1) {
+      setRecommendedWorkout(todaysWorkouts[currentWorkoutIndex + 1]);
+    } else {
+      setRecommendedWorkout(todaysWorkouts[0]);
+    }
+  };
+
+  // Increment the recommended warmup
+  const incrementRecommendedWarmup = () => {
+    const currentWarmupIndex = warmups.indexOf(recommendedWarmup);
+
+    if (currentWarmupIndex < warmups.length - 1) {
+      setRecommendedWarmup(warmups[currentWarmupIndex + 1]);
+    } else {
+      setRecommendedWarmup(warmups[0]);
+    }
+  }
+
+  // Increment the recommended recovery
+  const incrementRecommendedRecovery = () => {
+    const currentRecoveryIndex = recovery.indexOf(recommendedRecovery);
+
+    if (currentRecoveryIndex < recovery.length - 1) {
+      setRecommendedRecovery(recovery[currentRecoveryIndex + 1]);
+    } else {
+      setRecommendedRecovery(recovery[0]);
+    }
+  }
+
+  // Increment the recommended stretch
+  const incrementRecommendedStretch = () => {
+    const currentStretchIndex = stretches.indexOf(recommendedStretch);
+
+    if (currentStretchIndex < stretches.length - 1) {
+      setRecommendedStretch(stretches[currentStretchIndex + 1]);
+    } else {
+      setRecommendedStretch(stretches[0]);
+    }
+  }
+
+  // On mount, load all workouts, generate today's workouts, and set the recommended workout
   useEffect(() => {
+    // Get all workout data from the database
     const getWorkoutData = async () => {
       const data = await getAllWorkoutData();
-      setAllWorkoutData(data);
+      setCachedWorkoutData(data);
+      setWarmups(
+        data.filter((workout) => workout.category.includes("Warm Up"))
+      );
+      setRecovery(
+        data.filter((workout) => workout.category.includes("Recovery"))
+      );
+      setStretches(
+        data.filter((workout) => workout.category.includes("Stretch"))
+      );
+      return data;
+    };
+
+    // Generate today's workouts and recommended workout based on schedule
+    const generateTodaysWorkouts = (workoutData) => {
+      const currentDate = new Date();
+
+      // Filter the workouts based on the current day and schedule
+      const workoutList = workoutData.filter(
+        (workout) =>
+          workout.group.includes(
+            // Javascript uses 0-6 for Sunday-Saturday
+            currentWorkoutSchedule[currentDate.getDay()].group
+          ) &&
+          workout.category.includes(
+            // Javascript uses 0-6 for Sunday-Saturday
+            currentWorkoutSchedule[currentDate.getDay()].category
+          )
+      );
+
+      // Create a "score" for each workout based on watch count and rating
+      // workoutList.forEach((workout) => {
+      //   workout.score = workout.rating - workout.watchCount;
+      // });
+
+      // Sort the workouts by score
+      // workoutList.sort((a, b) => (a.score > b.score ? -1 : 1));
+
+      // Do the same for the warmups, recovery, and stretches
+      // const warmupList = workouts.filter(workout => workout.category.includes("Warm Up"));
+      // const recoveryList = workouts.filter(workout => workout.category.includes("Recovery"));
+      // const stretchList = workouts.filter(workout => workout.category.includes("Stretch"));
+
+      // warmupList.forEach((workout) => {
+      //   workout.score = workout.rating - workout.watchCount;
+      // }
+      // );
+      // warmupList.sort((a, b) => (a.score > b.score ? -1 : 1));
+
+      // recoveryList.forEach((workout) => {
+      //   workout.score = workout.rating - workout.watchCount;
+      // }
+      // );
+      // recoveryList.sort((a, b) => (a.score > b.score ? -1 : 1));
+
+      // stretchList.forEach((workout) => {
+      //   workout.score = workout.rating - workout.watchCount;
+      // }
+      // );
+      // stretchList.sort((a, b) => (a.score > b.score ? -1 : 1));
+
+      // Set the recommended warmup, recovery, and stretch to the first workout in the list
+      // setRecommendedWarmup(warmupList[0]);
+      // setRecommendedRecovery(recoveryList[0]);
+      // setRecommendedStretch(stretchList[0]);
+      setRecommendedWarmup(
+        workoutData.filter((workout) => workout.category.includes("Warm Up"))[0]
+      );
+      setRecommendedRecovery(
+        workoutData.filter((workout) =>
+          workout.category.includes("Recovery")
+        )[0]
+      );
+      setRecommendedStretch(
+        workoutData.filter((workout) => workout.category.includes("Stretch"))[0]
+      );
+
+      setTodaysWorkouts(workoutList);
+
+      // Set the recommended workout to the first workout in the list
+      setRecommendedWorkout(workoutList[0]);
+    };
+
+    // Generate today's workouts and recommended workout
+    // getWorkoutData().then((data) => {
+    //   generateTodaysWorkouts(data);
+    // });
+    if(cachedWorkoutData) {
+      console.log("cached!");
+      generateTodaysWorkouts(cachedWorkoutData);
+    } else {
+      console.log("grab from db!");
+      getWorkoutData().then((data) => {
+        generateTodaysWorkouts(data);
+      });
     }
 
-    getWorkoutData();
-  }
-  , []);
+  }, [currentWorkoutSchedule, cachedWorkoutData]);
 
-  return {allWorkoutData}
-}
-
-
+  return {
+    allWorkoutData,
+    recommendedWorkout,
+    todaysWorkouts,
+    warmups,
+    recovery,
+    stretches,
+    recommendedWarmup,
+    recommendedRecovery,
+    recommendedStretch,
+    currentWorkoutSchedule,
+    incrementRecommendedRecovery,
+    incrementRecommendedStretch,
+    incrementRecommendedWarmup,
+    switchCurrentWorkoutSchedule,
+    incrementRecommendedWorkout,
+    workoutScheduleAlt,
+  };
+};
 
 // import { useEffect, useState } from 'react';
 // import { db } from '../Config/firebase';
@@ -53,7 +215,6 @@ export const useGetWorkoutData = () => {
 
 // export const useGetWorkoutData = () => {
 //   const [allWorkoutData, setAllWorkoutData] = useState();
-
 
 //   useEffect(() => {
 //     const getWorkoutData = async () => {
@@ -87,10 +248,8 @@ export const useGetWorkoutData = () => {
 //           localStorage.setItem('workouts', JSON.stringify(item));
 //         });
 
-
 //         // console.log('Workout data retrieved from db');
 //         // console.log(allWorkoutData);
-
 
 //       } catch (error) {
 //         console.error(error);
@@ -120,7 +279,6 @@ export const useGetWorkoutData = () => {
 //       setAllWorkoutData(JSON.parse(cachedWorkoutData));
 //       return;
 //     }
-
 
 //     getWorkoutData();
 //   }, []);
