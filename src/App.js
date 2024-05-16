2ad2cef24e3dd14aab3fd76c2943ebb7c31ac8e5
bbgard <bgardiner@students.federation.edu.au>
2024-05-16 12:53:26 +1000

    Refactor App.js to use QueryClient with 60s stale value

--- src/App.js
+++ src/App.js
@@ -16,7 +16,15 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 
 function App() {
-  const queryClient = new QueryClient();
+  // const queryClient = new QueryClient();
+  const queryClient = new QueryClient({
+    defaultOptions: {
+      queries: {
+        // globally default to 60 seconds
+        staleTime: 1000 * 60,
+      },
+    },
+  })
   // const workoutData  = useGetWorkoutData();
 
   return (
